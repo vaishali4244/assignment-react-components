7e58f7c4547e4f5d3e8805f2356d729bb78d77e3
VAISHALI <vaishali120895@gmail.com>
2024-07-01 20:05:50 +0530

    first commit

---
+++ src/components/class.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Block extends React.Component{
+    render(){
+        return(
+            <div className="class">
+                <h2>I am Class Component.</h2>
+            </div>
+        )
+    }
+}
+
+export default Block;
---
+++ src/components/functional.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Functional = () => {
+  return (
+    <div className="function">
+      <h1>I am Functional Component.</h1>
+    </div>
+  );
+};
+
+export default Functional;
